1bdee14215c1ea28becbb22dddff7dfae6cb20c2
Shifat Naznin Nipa <shifatnaznin11@gmail.com>
2021-08-04 04:01:44 +0600

    update

--- resources/views/layouts/user-information-submit.blade.php
+++ resources/views/layouts/user-information-submit.blade.php
@@ -147,7 +147,7 @@
                     .then(function (response) {
                         // handle success
                         if (response.data.api_response_message == "SUCCESS") {
-                            return response;
+                          console.log(response.data);
                         } else {
                             swal({
                                 title: "Error",
