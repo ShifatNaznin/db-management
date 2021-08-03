8d711622a264950e6227a86b5138c1190c62ef8a
Shifat Naznin Nipa <shifatnaznin11@gmail.com>
2021-08-04 02:53:57 +0600

    new file update

--- resources/views/layouts/user-information.blade.php
+++ resources/views/layouts/user-information.blade.php
@@ -14,7 +14,6 @@
         <th>Division</th>
         <th>Status</th>
         <th>Ammount</th>
-        {{-- <th>Action</th> --}}
       </tr>
     </thead>
     <tbody>
@@ -31,11 +30,6 @@
         <td>{{ $item->status }}</td>
         <td>{{ $item->ammount }}</td>
 
-        {{-- <td>
-          <div class="send">
-            <a href="{{route('user_payment_information',$item->registration_number)}}" class="btn btn-theme" type="submit"> Payment</a>
-          </div>
-        </td> --}}
       </tr>
 
       @else
@@ -45,10 +39,7 @@
         <tr>
           <th>S.No.</th>
           <th>Registration.No.</th>
-          {{-- <th>Full Name</th>
-          <th>Mobile</th>
-          <th>Email</th>
-          <th>Division</th> --}}
+
           <th>Status</th>
           <th>Ammount</th>
           <th>Action</th>
@@ -61,10 +52,7 @@
 
         <td>{{ $item->id }}</td>
         <td>{{ $item->registration_number }}</td>
-        {{-- <td>{{ $item->full_name }}</td>
-        <td>{{ $item->phone }}</td>
-        <td>{{ $item->email }}</td>
-        <td>{{ $item->division }}</td> --}}
+
         <td>{{ $item->status }}</td>
         <td>{{ $item->ammount }}</td>
 
